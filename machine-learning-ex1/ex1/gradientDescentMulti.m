cc6b9e654fcd3e4570c4ab59770c8fc912b760a9
Swapneel Datta <swapneeldatta@dattus.com>
2018-05-08 15:51:49 +0530

    Finished everything Week 2

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -17,18 +17,10 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
+%	theta=theta - alpha*(sum(X*theta-y)/m)
 
-
-
-
-
-
-
-
-
-
+	theta=theta - (alpha/m)*(sum(X.*(X*theta-y)))';
     % ============================================================
-
     % Save the cost J in every iteration    
     J_history(iter) = computeCostMulti(X, y, theta);
 
